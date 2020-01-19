20416d182f93e57b558b58acdf019236f48827d1
Parth Koshta <parth.k@theghack.in>
2020-01-19 15:44:39 +0530

    control ui

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {SizeMetrics} from './src/Constants';
 import {GameEngine} from 'react-native-game-engine';
-import { Head, GameLoop } from './src/Components';
+import {Head, GameLoop} from './src/Components';
 
 class App extends Component {
   constructor() {
@@ -26,13 +26,57 @@ class App extends Component {
               flex: null,
             },
           ]}
-
-          entities = {{
-            head: { position: [0,  0], xspeed:1, yspeed:0, nextMove: 1, updateFrequency: 1, size: SizeMetrics.CELL_SIZE, renderer: <Head />}
+          entities={{
+            head: {
+              position: [0, 0],
+              xspeed: 1,
+              yspeed: 0,
+              nextMove: 1,
+              updateFrequency: 1,
+              size: SizeMetrics.CELL_SIZE,
+              renderer: <Head />,
+            },
           }}
-
-          systems = {[GameLoop]} 
+          systems={[GameLoop]}
         />
+        <View
+          style={{
+            marginTop: 20,
+          }}>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <TouchableOpacity>
+              <View style={styles.triangle} />
+            </TouchableOpacity>
+          </View>
+
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row',
+            }}>
+            <TouchableOpacity>
+              <View style={[styles.triangle, styles.left]} />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <View style={[styles.triangle, styles.right]} />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <TouchableOpacity>
+              <View style={[styles.triangle, styles.down]} />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -44,6 +88,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  triangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 20,
+    borderRightWidth: 20,
+    borderBottomWidth: 40,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'red',
+    marginVertical: 10,
+    marginHorizontal: 20,
+  },
+  right: {
+    transform: [{rotate: '90deg'}],
+    marginLeft: 40,
+  },
+  left: {
+    transform: [{rotate: '-90deg'}],
+  },
+  down: {
+    transform: [{rotate: '180deg'}],
   },
 });
 
